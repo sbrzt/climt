17869bfcbf0823d95df7c57161345d69aaf7b3d7
holyyeti <sebastian.barzaghi@outlook.com>
2024-06-07 01:00:08 +0200

    feat: char count, word count, word details

--- analyze.py
+++ analyze.py
@@ -1,28 +1,50 @@
-import nltk, statistics
+import nltk, statistics, string
+from collections import Counter
+from nltk.corpus import stopwords
+
+def preprocess_text(text):
+    # Convert text to lowercase
+    text = text.lower()
+    # Remove punctuation
+    text = text.translate(str.maketrans('', '', string.punctuation))
+    # Tokenize text into words
+    words = nltk.word_tokenize(text)
+    # Remove stopwords
+    stop_words = set(stopwords.words('english'))
+    words = [word for word in words if word not in stop_words]
+    return words
 
 def analyze_text(text):
     # Tokenize text into words and sentences
-    words = nltk.word_tokenize(text)
-    sentences = nltk.sent_tokenize(text)
+    words = preprocess_text(text)
     
     # Character count
     char_count = len(text)
-    
+
     # Word count
     word_count = len(words)
-    
-    # Sentence count
-    sentence_count = len(sentences)
 
-    # Average length of words
-    avg_word_length = statistics.mean(len(word) for word in words) if words else 0
+    # Top 50 words
+    word_freq = Counter(words).most_common(50)
 
+    pos_tags = dict(nltk.pos_tag(words))
+
+    # Calculate frequency in terms of percent and get POS tags
+    word_details = []
+    for word, count in word_freq:
+        frequency_percent = (count / word_count) * 100
+        pos_tag = pos_tags.get(word, 'N/A')
+        word_details.append({
+            'word': word,
+            'occurrences': count,
+            'frequency_percent': frequency_percent,
+            'pos_tag': pos_tag
+        })
 
     analysis = {
         'char_count': char_count,
         'word_count': word_count,
-        'sentence_count': sentence_count,
-        'average_word_length': avg_word_length
+        'word_details': word_details
     }
 
     return analysis
--- main.py
+++ main.py
@@ -6,12 +6,19 @@ def main():
     Main function that initializes the analyzer. 
     '''
     parser = argparse.ArgumentParser(description="Analyze a text file and generate reports in various formats.")
-    parser.add_argument("input_file", help="Path to the input text file")
-    parser.add_argument("--text", help="Output path for the text report")
+    parser.add_argument("input", help="Path to the input text file")
+    parser.add_argument("--output", help="Output path for the text report")
+    #parser.add_argument(
+    # '--output_format', 
+    # type=str, 
+    # choices=['txt', 'csv', 'rdf'], 
+    # default='txt', 
+    # help="Output report format"
+    # )
 
     args = parser.parse_args()
 
-    with open(args.input_file, 'r') as file:
+    with open(args.input, 'r') as file:
         text = file.read()
         
     analysis = analyze.analyze_text(text)
